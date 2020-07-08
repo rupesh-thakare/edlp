9096d01dd17fa91d4f93f1198f57c61c90b63b47
rupesh-thakare <rupesh@edlp.app>
2020-07-08 15:49:35 +0530

    db update script for category

--- db_update.py
+++ db_update.py
@@ -1,5 +1,19 @@
 import csv
 
+def update_category(catalog_file, db, Catalog):
+    with open(catalog_file, mode='r') as catalog_file:
+        csv_reader = csv.DictReader(catalog_file)
+        for record in csv_reader:
+            category_id = record['category_id']
+            if not Catalog.query.get(category_id):
+                try:
+                    db.session.add(Catalog(
+                        category_id=category_id,
+                        category_name=record['category_name'],
+                    ))
+                    db.session.commit()
+                except Exception as e:
+                    print(e)
 
 def update_catalog(catalog_file, db, Catalog):
     with open(catalog_file, mode='r') as catalog_file:
