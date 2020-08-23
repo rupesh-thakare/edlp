d75fc30b437a73077611aec32280357da7138cca
rupesh-thakare <rupesh@edlp.app>
2020-08-23 20:33:53 +0530

    removed instance settings added to git

--- instance/settings.py
+++
@@ -1,8 +0,0 @@
-import os 
-
-database_user = 'rupeshthakare'
-database_password = 'dbpassword'
-
-DEBUG = False
-
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or f'mysql+mysqlconnector://{database_user}:{database_password}@rupeshthakare.mysql.pythonanywhere-services.com:3306/rupeshthakare$default'
