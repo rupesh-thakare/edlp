027c04ad0fefe4b524169c7df1ba29e7b7e4615c
rupesh-thakare <rupesh@edlp.app>
2020-08-24 11:45:34 +0530

    added sqlalchemy pool recycle

--- config/settings.py
+++ config/settings.py
@@ -10,5 +10,7 @@ basedir = os.path.abspath(os.path.dirname(__package__))
 db_path = os.path.join(basedir, 'data-dev.sqlite')
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_POOL_RECYCLE = 289 #PythonAnywhere sets timeout of sessions to 5 minutes. So to avoid disconnects setting this to less than 300 seconds
+SQLALCHEMY_POOL_TIMEOUT = 20
 
 REMEMBER_COOKIE_DURATION = timedelta(days=90)
